f13f105c99b525d94f2b61254947b496853955a5
erv <ervinmitra@gmail.com>
2019-09-30 09:27:16 -0700

    skrt skrt

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const axios = require('axios');
 
 const req = axios(`GET https://therundown-therundown-v1.p.rapidapi.com/sports/2/events`);
+
+req.query({
+    'include': [
+        'all_periods',
+        'scores'
+    ]
+});
+
+req.headers({
+    'x-rapidapi-host': 'therundown-therundown-v1.p.rapidapi.com',
+
+});
