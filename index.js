5d1669f68c8e3ca8f53af69d39b0c26e35d3eb0a
erv <ervinmitra@gmail.com>
2019-10-01 09:55:37 -0700

    pull data from 3rd party API

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const axios = require('axios');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '/.env') });
 
-const req = axios(`GET https://therundown-therundown-v1.p.rapidapi.com/sports/2/events`);
+const pull = async () => {
+    const data = await axios.get(`https://${process.env.API_HOST}/sports/2/events/2019-09-29`, {
+        query: {
+            'include': [
+                'all_periods',
+                'scores'
+            ],
+            'offset': '0',
+        },
+        headers: {
+            'x-rapidapi-host': `${process.env.API_HOST}`,
+            'x-rapidapi-key': `${process.env.API_KEY}`,
+        },
+    });
+        /**
+        * data.events -> fucking array
+        * data.events[index].teams_normalized[0].abbreviation (away team abbr)
+        * data.events[index].teams_normalized[1].abbreviation (home team abbr)
+        * data.events[index].score.score_away
+        * data.events[index].score.score_home
+        */
+    // const games = [];
+    // for(let i = 0; i < data.data.events.length; i++) {
+    //     games.push({
+    //         team_away: data.data.events[i].teams_normalized[0].abbreviation,
+    //         team_home: data.data.events[i].teams_normalized[1].abbreviation,
+    //         // score_away: data.data.events[i].score.score_away,
+    //         // score_home: data.data.events[i].score.score_home,
+    //         score_temp: data.data.events[i].score,
+    //     });
+    // }
+    // return games;
+    return data.data.events;
+}
 
-req.query({
-    'include': [
-        'all_periods',
-        'scores'
-    ]
-});
-
-req.headers({
-    'x-rapidapi-host': 'therundown-therundown-v1.p.rapidapi.com',
-    'x-rapidapi-key': ``,
-});
+pull().then((data) => console.log(data));
